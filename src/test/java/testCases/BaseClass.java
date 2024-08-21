535417c0d77615cf32feeb60475a1b32c5312b19
sri...... <srikanthv1709@gmail.com>
2024-08-21 13:20:00 +0530

    Update BaseClass.java

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -1,40 +1,74 @@
-package testCases;
+package testBase;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager; //log4j2
+import org.apache.logging.log4j.Logger; //log4j2
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
-	public WebDriver driver= new ChromeDriver(); 
-	
+	public WebDriver driver;
+	public Logger logger; // log4j2
+	public Properties properties;
+
 	@BeforeClass
-	public void setup() {
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String br) throws IOException {
+		//Loading config.properties file
+		FileReader file = new FileReader(".//src//test//resources//config.properties");
+		properties = new Properties();
+		properties.load(file);
+		logger = LogManager.getLogger(this.getClass()); // this.getClass() will dynamically getting the class name
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		/*case "firefox":
+			driver = new FirefoxDriver();
+			break;
+			*/
+		default:
+			System.out.println("Invalid browser name");
+			return;
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://tutorialsninja.com/demo/");		
+		driver.get(properties.getProperty("appURL"));// reading URL from properties file
 	}
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
-	
+
 	public String randomString() {
 		String generatedString = RandomStringUtils.randomAlphabetic(5);
-		return generatedString;		
+		return generatedString;
 	}
+
 	public String randomNumber() {
 		String generatedNumber = RandomStringUtils.randomNumeric(10);
-		return generatedNumber;		
+		return generatedNumber;
 	}
+
 	public String randomAlphaNumeric() {
 		String generatedString = RandomStringUtils.randomAlphabetic(3);
 		String generatedNumber = RandomStringUtils.randomNumeric(3);
-		return (generatedString+"#"+generatedNumber);		
+		return (generatedString + "#" + generatedNumber);
 	}
 
 }
