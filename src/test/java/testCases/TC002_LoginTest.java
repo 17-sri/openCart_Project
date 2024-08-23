835702c8e8f8c897d98cd9616c768eb31404ac89
Admin <Admin@DEMON-PC>
2024-08-23 18:21:42 +0530

    push

---
+++ src/test/java/testCases/TC002_LoginTest.java
@@ -0,0 +1,38 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC002_LoginTest extends BaseClass {
+	@Test
+	public void verify_login() {
+
+		logger.info("...starting TC002_LoginTest... ");
+		try {
+			//HomePage
+			HomePage homePage = new HomePage(driver);
+			homePage.clickMyAccount();
+			homePage.clickLogin();
+			//LoginPage
+			LoginPage loginPage = new LoginPage(driver);
+			loginPage.setEmail(properties.getProperty("email"));
+			loginPage.setPassword(properties.getProperty("password"));
+			loginPage.clickLogin();
+			//MyAccountPage
+			MyAccountPage myAcc = new MyAccountPage(driver);
+			boolean targetPage = myAcc.isMyAccountPageExists();
+			Assert.assertTrue(targetPage);//Assert.assertEquals(targetPage, true, "Login Failed");
+		}
+		catch(Exception e) {
+			Assert.fail();
+		}
+		logger.info("...finished TC002_LoginTest... ");
+
+	}
+
+}
---
+++ src/test/java/utilities/DataProviders.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+	//DataProvider 1
+	@DataProvider(name = "LoginData")
+	public String [][] getData() throws IOException{
+		String path = ".\\testData\\Opencart_LoginData.xlsx";//taking Excel file from tastData
+		ExcelUtility xlUtil = new ExcelUtility(path);//creating an object for ExcelUtility
+		int totalRows = xlUtil.getRowCount("Sheet1");
+		int totalCols = xlUtil.getCellCount("Sheet1", 1);
+		String loginData[][] = new String[totalRows][totalCols];// created for two dimension array which can store rows and columns
+		for(int i=1;i<=totalRows;i++) {
+			for(int j=0;j<totalCols;j++) {
+				loginData[i-1][j] = xlUtil.getCellData("Sheet1", i, j);//1,0
+			}
+		}
+	return loginData;//returning two dimension array	
+	}
+	
+	//DataProvider 2
+	
+	//DataProvider 3
+}
